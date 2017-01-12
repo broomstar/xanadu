01a8c033ff61195ca62ceb0a2f252c76f8cd857b
xBuyax <zszabo@t-online.de>
2017-01-12 11:33:23 +0100

    *improved mob movement packet*

--- xanadu/mob_handler.cpp
+++ xanadu/mob_handler.cpp
@@ -44,55 +44,63 @@ void Player::handle_mob_movement()
 
 		switch (action)
 		{
-		case 0: // Normal up/down/left/right movement
+		case 0:
 		case 5:
 		case 17:
 			mob_position_x = read<short>();
 			mob_position_y = read<short>();
-			skip_bytes(6);
+			skip_bytes(2);
+			skip_bytes(2);
+			skip_bytes(2);
 			mob_stance = read<signed char>();
 			mob_foothold = read<short>();
 			break;
-		case 15: // Jump down
+		case 15:
 			mob_position_x = read<short>();
 			mob_position_y = read<short>();
-			skip_bytes(8);
+			skip_bytes(2);
+			skip_bytes(2);
+			skip_bytes(2);
+			skip_bytes(2);
 			mob_stance = read<signed char>();
 			mob_foothold = read<short>();
 			break;
-		case 1: // Jumping
-		case 2: // Jumping/knockback?
-		case 6: // Flash Jump
-		case 12: // Horntail knockback
-		case 13: // Recoil Shot
+		case 1:
+		case 2:
+		case 6:
+		case 12:
+		case 13:
+		case 16:
+		case 18:
+		case 19:
 		case 20:
+		case 22:
 			mob_position_x = read<short>();
 			mob_position_y = read<short>();
 			mob_stance = read<signed char>();
 			mob_foothold = read<short>();
 			break;
 		case 3:
-		case 4: // Teleport
-		case 7: // Assaulter
-		case 8: // Assassinate
-		case 9: // Rush
-		case 11: // Chair
+		case 4:
+		case 7:
+		case 8:
+		case 9:
+		case 11:
 			mob_position_x = read<short>();
 			mob_position_y = read<short>();
 			skip_bytes(2);
 			mob_foothold = read<short>();
 			mob_stance = read<signed char>();
 			break;
-		case 10: // Falling of some kind
+		case 10:
 			skip_bytes(1);
 			break;
 		case 14:
-			skip_bytes(6);
+			skip_bytes(2);
+			skip_bytes(2);
+			skip_bytes(2);
 			mob_stance = read<signed char>();
 			mob_foothold = read<short>();
-			break;
-		case 16:
-			skip_bytes(7);
 			break;
 		default:
 			break;
